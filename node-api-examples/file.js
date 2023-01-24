31bb37f5c410bb421b5e517ca78d1bed6d5a5c57
Gracelyn Prajna <gracelynprajna@gmail.com>
2023-01-24 18:46:06 -0500

    classwork update

--- node-api-examples/file.js
+++ node-api-examples/file.js
@@ -3,7 +3,26 @@ const fs = require("fs");
 (function () {
   const fileContent = fs.readFileSync("./index.txt");
 
-  console.log(fileContent); //Should print whatever is put into the file
+  console.log(fileContent.toString()); //Should print whatever is put into the file
 })();
 
 //For the next one, let's open a folder and read all it's files and the file's contents;
+(function() {
+  fs.readdir("./", function(err, content){
+    if(err){
+      throw err;
+    } else{
+      content.forEach(fileOrFolder => {
+        const splitName = fileOrFolder.split(".");
+        
+        if (splitName.length >=2){
+          console.log("This is a file");
+        }else {
+          const folderContents = fs.readdirSync(fileOrFolder);
+        }
+      })
+    }
+  })
+
+
+})();
